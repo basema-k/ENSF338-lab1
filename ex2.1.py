6b5bca49ecc0bef1a92faadd6e5e481f7e2aae74
basema-k <basema.khan1@ucalgary.ca>
2026-02-03 19:52:16 -0700

    Update ex2.1.py

--- ex2.1.py
+++ ex2.1.py
@@ -1,3 +1,22 @@
+import sys
+import math
+def do_stuff():
+    a = float(sys.argv[1])
+    b = float(sys.argv[2])
+    c = float(sys.argv[3])
+
+    d = b**2 - 4*a*c
+    if d > 0:
+        root1 = (-b + math.sqrt(d)) / (2*a)
+        root2 = (-b - math.sqrt(d)) / (2*a)
+        print(f'The solutions are: {root1}, {root2}') # fixed quotation mark
+    elif d == 0:
+        root = -b / (2*a)
+        print(f'The solution is: {root}') # fixed quotation mark
+    else:
+        print('There are no real solutions.')
+do_stuff()
+
 '''
 Part 2.1 - Find the Error
 i) The code shown is meant to find the roots of a quadratic equation 
@@ -5,5 +24,6 @@ i) The code shown is meant to find the roots of a quadratic equation
     It takes in values for a, b, and c as the coefficients of the 3 terms.
 ii) The error is contained within the print statements, 
     where the opening and closing quotation marks are not the same.
+
 
 '''
